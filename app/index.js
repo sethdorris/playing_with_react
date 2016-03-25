91de24e47a36b7e41ce2d2430fa6c20126b050f2
sethdorris <sethc.dorris@gmail.com>
2016-03-25 15:37:17 -0600

    Building nested components

--- app/index.js
+++ app/index.js
@@ -1,12 +1,48 @@
 import React from "react";
-import ReactDOM from "react-dom"
+import ReactDOM from "react-dom";
 
-class HelloWorld extends React.Component {
+let User_Data = {
+	name: "Seth Dorris",
+	username: "sethdorris",
+	image: "https://avatars1.githubusercontent.com/u/12013667?v=3&s=460"
+}
+
+class ProfilePic extends React.Component {
 	render() {
 		return (
-			<div> Hello ES2015 React App </div>
+			<img src={this.props.imageUrl.image} style={{height: 100, width: 100}} />
 		)
 	}
 }
 
-ReactDOM.render(<HelloWorld />, document.getElementById("app"));
+class ProfileLink extends React.Component  {
+	render() {
+		return (
+			<div>
+				<a href={'https://www.github.com' + this.props.username.username}>{this.props.user.username}</a>
+			</div>
+		)
+	}
+}
+
+class Avatar extends React.Component {
+	render() {
+		return (
+			<div>
+				<ProfilePic imageUrl={this.props.user.image}/>
+				<ProfileLink username={this.props.user.username} />
+			</div>
+		)
+	}
+}
+
+class HelloWorld extends React.Component {
+	render() {
+		console.log(this.props);
+		return (
+			<div> Hello {this.props.name}, Welcome to ES2015 React App </div>
+		)
+	}
+}
+
+ReactDOM.render(<Avatar user={User_Data}/>, document.getElementById("app"));
