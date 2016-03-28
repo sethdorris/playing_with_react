2338b3f17e8125d34cf30b4dd98b409d0eca57e7
sethdorris <sethc.dorris@gmail.com>
2016-03-28 15:36:01 -0600

    added route history

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import * as ReactRouter from 'react-router';
-import {Route, Router, IndexRoute} from 'react-router';
+import {Route, Router, IndexRoute, hashHistory} from 'react-router';
 import Main from '../components/Main';
 import Home from '../components/Home';
 
 const routes = (
-    <Router>
+    <Router history={hashHistory}>
         <Route path="/" component={Main}>
             <Route path="/home" component={Home} />
         </Route>
