a0eca30ce4ecd0373c57c32e14ad76e980580442
sethdorris <sethc.dorris@gmail.com>
2016-03-28 14:37:30 -0600

    Added router config file

---
+++ app/config/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactRouter from 'react-router';
+const Router = ReactRouter.Router;
+const Route = ReactRouter.Route;
+const IndexRoute = ReactRouter.IndexRoute;
+import Main from '../components/Main';
+import Home from '../components/Home';
+
+const routes = (
+    <Router>
+        <Route path="/" component={Main}>
+            <Route path="/home" component={Home} />
+        </Route>
+    </Router>
+);
+
+module.exports = routes;
