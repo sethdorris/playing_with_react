cd5c777d6429a6dabb4c7f571a9b83b427120f8c
sethdorris <sethc.dorris@gmail.com>
2016-03-28 14:37:16 -0600

    Added separate component files and folder to test react router

---
+++ app/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Main extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello From Main!
+                {this.props.children}
+            </div>
+            )
+    }
+};
+
+module.exports = Main;
