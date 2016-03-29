e5b227e5ee13e803307e1e949649c41eb828a523
sethdorris <sethc.dorris@gmail.com>
2016-03-29 11:13:26 -0600

    created containers folder and created a promptcontainer component skeleton

---
+++ app/containers/PromptContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PromptContainer extends React.Component {
+    render() {
+        return (
+            <div className="jumbotron col-sm-6 col-sm-offset-3 text-center">
+            
+            </div>
+        )
+    }
+}
+
+module.exports = PromptContainer;
