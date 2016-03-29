2140d94bb8d0cededeb2a314967f11f00e66f64c
sethdorris <sethc.dorris@gmail.com>
2016-03-29 16:01:18 -0600

    Adding button functions to the component and setting initial state

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -1,13 +1,58 @@
 import React from 'react';
+import Styles from '../styles';
 
 class PromptContainer extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            username: ''
+        };
+    }
+    onUpdateUser(e) {
+        this.setState({
+            username: e.target.value
+        })
+    }
+
+    onSubmitUser(e) {
+        e.preventDefault();
+        var username = this.state.username;
+        this.setState({
+            username: ''
+        });
+    }
+
+    if (this.props.routeParams.playerOne) {
+
+    } else {
+
+    }
+
     render() {
         return (
-            <div className="jumbotron col-sm-6 col-sm-offset-3 text-center">
-            
+            <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={Styles.transparentBg}>
+                <h1>{this.props.route.header}</h1>
+                <div className="col-sm-12">
+                    <form onSubmit={this.onSubmitUser}>
+                        <div className="form-group">
+                            <input 
+                                className="form-control"
+                                placeholder="Github Username"
+                                onChange={this.onUpdateUser}
+                                value={this.state.username}
+                                type="text" />
+                        </div>
+                        <div className="form-group col-sm-4 col-sm-offset-4">
+                        <button 
+                            className="btn btn-block btn-success"
+                            type="submit">Continue
+                        </button>
+                        </div>
+                    </form>
+                </div>
             </div>
         )
-    }
-}
+                }
+                }
 
-module.exports = PromptContainer;
+export default PromptContainer;
