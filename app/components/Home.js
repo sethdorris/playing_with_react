bab2a0988369e2952d0827709ef9a616d5ae40ad
sethdorris <sethc.dorris@gmail.com>
2016-03-29 15:59:48 -0600

    Updating Home layout

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Styles from '../styles';
+import * as ReactRouter from 'react-router';
+const Link = ReactRouter.Link;
 
 class Home extends React.Component {
     render() {
         return (
-            <div> Hello from Home! </div>
+            <div className="jumbotron col-sm-12 text-center" style={Styles.transparentBg} >
+                <h1>Github Battle</h1>
+                <p className="lead">Some Fancy Motto</p>
+                <Link to="/playerOne">
+                    <button type="button" className="btn btn-lg btn-success">Get Started</button>
+                </Link>
+             </div>
             )
     }
 };
