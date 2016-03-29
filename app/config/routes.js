2c6459474d98afd54acdcbfc4b2444a26eb738d1
sethdorris <sethc.dorris@gmail.com>
2016-03-29 16:00:44 -0600

    Updated routing to include playerone and playertwo route params

--- app/config/routes.js
+++ app/config/routes.js
@@ -3,11 +3,14 @@ import * as ReactRouter from 'react-router';
 import {Route, Router, IndexRoute, hashHistory} from 'react-router';
 import Main from '../components/Main';
 import Home from '../components/Home';
+import PromptContainer from '../containers/PromptContainer';
 
 const routes = (
     <Router history={hashHistory}>
         <Route path="/" component={Main}>
-            <Route path="/home" component={Home} />
+            <IndexRoute component={Home} />
+            <Route path="playerOne" header="Player One" component={PromptContainer} />
+            <Route path="playerTwo/:playerOne" header="Player Two" component={PromptContainer} />
         </Route>
     </Router>
 );
