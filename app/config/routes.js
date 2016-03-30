cb2a5db1ef5964e1db3998ddc99aba3242fd41de
sethdorris <sethc.dorris@gmail.com>
2016-03-30 16:27:18 -0600

    Adding route path for battle

--- app/config/routes.js
+++ app/config/routes.js
@@ -4,6 +4,7 @@ import {Route, Router, IndexRoute, hashHistory} from 'react-router';
 import Main from '../components/Main';
 import Home from '../components/Home';
 import PromptContainer from '../containers/PromptContainer';
+import ConfirmBattleContainer from '../containers/ConfirmBattleContainer';
 
 const routes = (
     <Router history={hashHistory}>
@@ -11,6 +12,7 @@ const routes = (
             <IndexRoute component={Home} />
             <Route path="playerOne" header="Player One" component={PromptContainer} />
             <Route path="playerTwo/:playerOne" header="Player Two" component={PromptContainer} />
+			<Route path="battle" component={ConfirmBattleContainer} />
         </Route>
     </Router>
 );
