af6e8a1063715291e620d38366ff598d2abdbc1b
sethdorris <sethc.dorris@gmail.com>
2016-03-30 14:07:24 -0600

    Took out the render component

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Styles from '../styles';
+import Prompt from '../components/Prompt';
+const PropTypes = React.PropTypes;
 
 class PromptContainer extends React.Component {
     constructor() {
@@ -8,15 +9,17 @@ class PromptContainer extends React.Component {
             username: ''
         };
 		console.log(this);
+		this.handleUpdateUser = this.handleUpdateUser.bind(this);
+		this.handleSubmitUser = this.handleSubmitUser.bind(this);
     }
 
-    onUpdateUser(e) {
+    handleUpdateUser(e) {
         this.setState({
             username: e.target.value
         })
     }
 
-    onSubmitUser(e) {
+    handleSubmitUser(e) {
         e.preventDefault();
         var username = this.state.username;
         this.setState({
@@ -24,35 +27,25 @@ class PromptContainer extends React.Component {
         });
 
         if (this.props.routeParams.playerOne) {
-            console.log(this.context)
+            this.context.router.push({
+				pathname: '/battle',
+				query: {
+					playerOne: this.props.routeParams.playerOne,
+					playerTwo: this.state.username
+				}
+			})
         } else {
-            console.log(this.context)
+            this.context.router.push('/playerTwo/' + this.state.username);
         }
     }
 
     render() {
         return (
-            <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={Styles.transparentBg}>
-                <h1>{this.props.route.header}</h1>
-                <div className="col-sm-12">
-                    <form onSubmit={this.onSubmitUser}>
-                        <div className="form-group">
-                            <input 
-                                className="form-control"
-                                placeholder="Github Username"
-                                onChange={this.onUpdateUser}
-                                value={this.state.username}
-                                type="text" />
-                        </div>
-                        <div className="form-group col-sm-4 col-sm-offset-4">
-                        <button 
-                            className="btn btn-block btn-success"
-                            type="submit">Continue
-                        </button>
-                        </div>
-                    </form>
-                </div>
-            </div>
+			<Prompt
+				onSubmitUser={this.handleSubmitUser}
+				onUpdateUser={this.handleUpdateUser}
+				header={this.props.route.header}
+				username={this.state.username} />
         )
    }
 }
