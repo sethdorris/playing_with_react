dd84a9680a08f1d0488bbaa25222d0d69768d644
sethdorris <sethc.dorris@gmail.com>
2016-03-31 15:47:49 -0600

    Deleted hello from Main

--- app/components/Main.js
+++ app/components/Main.js
@@ -4,7 +4,6 @@ class Main extends React.Component {
     render() {
         return (
             <div>
-                Hello From Main!
                 {this.props.children}
             </div>
             )
