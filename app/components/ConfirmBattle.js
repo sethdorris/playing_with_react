bcdf46aa2036073547fe996340d9445386af387e
sethdorris <sethc.dorris@gmail.com>
2016-03-31 14:28:24 -0600

    Added User Details and User Details Components to the render method

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import Styles from '../styles/';
-const PropTypes = React.PropTypes;
+import React, {PropTypes} from 'react';
+import Styles from '../styles';
 import {Link} from 'react-router';
+import UserDetails from './UserDetails';
+import UserDetailsWrapper from './UserDetailsWrapper';
 
 
 function puke (object) {
@@ -12,30 +13,29 @@ function puke (object) {
 const ConfirmBattle = (props) => {
 	return props.isLoading === true
 	? <p> Loading! </p>
-	: <div> className="jumbotron col-sm-12 text-center" style={Styles.transparentBg}>
+	: <div className="jumbotron col-sm-12 text-center" style={Styles.transparentBg}>
 		<h1>Confirm Players</h1>
 		<div className="col-sm-8 col-sm-offset-2">
-			<div className="col-sm-6">
-				<p className="lead">Player 1 </p>
-				{puke(props.playersInfo[0])}
-			</div>
-			<div className="col-sm-6">
-				<p className="lead">Player 2 </p>
-				{puke(props.playersInfo[1])}
-			</div>
+			<UserDetailsWrapper header="Player One">
+				<UserDetails info={props.playersInfo[0]} />
+			</UserDetailsWrapper>
+			<UserDetailsWrapper header="Player 2">
+				<UserDetails info={props.playersInfo[1]} />
+			</UserDetailsWrapper>
 		</div>
 		<div className="col-sm-8 col-sm-offset-2">
-			<div className="col-sm-12" style={styles.space}>
+			<div className="col-sm-12" style={Styles.space}>
 				<button type="button" className="btn btn-lg btn-success" onClick={props.onInitiateBattle}>
 					Initiate Battle!
 				</button>
 			</div>
-			<div className="col-sm-12" style={styles.space}>
+			<div className="col-sm-12" style={Styles.space} >
 				<Link to="/playerOne">
 					<button type="button" className="btn btn-lg btn-danger">Reselect Players</button>
 				</Link>	
 			</div>
 		</div>
+	</div>
 }
 
 ConfirmBattle.propTypes = {
