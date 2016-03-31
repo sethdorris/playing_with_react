2d3158b9dc01ea10d9ee71fe14d63b72caf69c3a
sethdorris <sethc.dorris@gmail.com>
2016-03-31 15:48:40 -0600

    Added functions / algorithm for calculating scores

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -7,6 +7,34 @@ function getUserInfo(username) {
 	return axios.get('https://api.github.com/users/' + username + param);
 }
 
+function getRepos(username) {
+	return axios.get('https://api.github.com/users/' + username + '/repos' + param + '&per_page=100');
+}
+
+function getTotalStars(repos) {
+	return repos.data.reduce((prev, current) => {
+		return prev + current.stargazers_count
+	}, 0)
+}
+
+function getPlayersData(player) {
+	return getRepos(player.login)
+	.then(getTotalStars)
+	.then((totalStars) => {
+		return {
+			followers: player.followers,
+			totalStars: totalStars
+		}
+	})
+}
+
+function calculateScores(players) {
+	return [
+		players[0].followers * 3 + players[0].totalStars,
+		players[1].followers * 3 + players[1].totalStars
+	]
+}
+
 let helpers = {
 	getPlayersInfo: function(players) {
 		return axios.all(players.map((username) => {
@@ -16,6 +44,17 @@ let helpers = {
 				return user.data
 			});
 		}).catch((err) => {
+			console.warn('Error in getPlayersInfo', err);
+		})
+	},
+	
+	battle: function(players) {
+		let playerOneData = getPlayersData(players[0])
+		let playerTwoData = getPlayersData(players[1])
+		
+		return axios.all([playerOneData, playerTwoData])
+		.then(calculateScores)
+		.catch((err) => {
 			console.warn('Error in getPlayersInfo', err);
 		})
 	}
